3ffe1cf7bd4e086c797a34045c88ef8248abb2ab
leozhu <wjzqh1981@gmail.com>
2017-10-16 23:54:47 +0800

    add level 5 and level 6

---
+++ code/level_5.py
@@ -0,0 +1,19 @@
+﻿import pickle
+
+#打开文件
+pk_file=open("..\\data\\banner.p","rb")
+
+
+
+#将文件反序列化成对象
+data=pickle.load(pk_file)
+
+str=""
+
+#对输出做处理
+for list in data:
+    print(list)
+    for i in list:
+        str+=i[0]*i[1]
+    str+='\n'
+print (str)
---
+++ code/level_6.py
@@ -0,0 +1,24 @@
+﻿import zipfile
+zip = '..\\data\\channel.zip'
+z = zipfile.ZipFile(zip,'r')
+number = '90052'
+suffix = '.txt'
+comments = []
+while True:
+	if number is '':
+		break;
+	content = z.read(number+suffix)
+	comments.append(z.getinfo(number+suffix).comment)
+	content = content.split()
+	number = ''
+	for i in content:
+		if i.isdigit():
+			number = str(i)
+			break
+
+print "".join(comments)
+
+'''
+Printing the result forms the letters 'HOCKEY', but using that as a URL will tell you "it's in the air" and to look at the letters. 
+Indeed, the letters of the word hockey have been built up out of the letters O, X, Y, G, E, and N, and 'oxygen' leads to the next level.
+'''
